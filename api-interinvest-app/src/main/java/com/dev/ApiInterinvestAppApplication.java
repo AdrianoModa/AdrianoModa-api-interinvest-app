cc59b870bde1c1eaa3cefd26e137bb244ae68461
adrianomoda <adrianomodafeitosa@gmail.com>
2022-03-22 18:09:28 -0300

    Estrutura projeto com Maven

---
+++ api-interinvest-app/src/main/java/com/dev/ApiInterinvestAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInterinvestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInterinvestAppApplication.class, args);
+	}
+
+}
