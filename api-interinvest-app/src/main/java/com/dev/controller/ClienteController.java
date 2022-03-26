ff85b5ecabfdfebde8ca68236422e861eef584d8
adrianomoda <adrianomodafeitosa@gmail.com>
2022-03-26 17:42:53 -0300

    Cadastro de clientes

--- api-interinvest-app/src/main/java/com/dev/controller/ClienteController.java
+++ api-interinvest-app/src/main/java/com/dev/controller/ClienteController.java
@@ -2,8 +2,13 @@ package com.dev.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.dev.domain.Cliente;
@@ -20,6 +25,11 @@ public class ClienteController {
 	@GetMapping
 	public List<Cliente> listarTodos(){
 		return clienteService.buscarTodos();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> registrarCliente(@RequestBody @Validated Cliente cliente){
+		return new ResponseEntity<Cliente>(clienteService.cadastrar(cliente), HttpStatus.CREATED);
 	}
 
 }
